2045a339dca277bf9fe8aaadf86446a96e2c59a4
Abhijit Nath <abhijitnath1a1@gmail.com>
2025-09-25 10:54:45 +0530

    Project Setup and shadcn installation

---
+++ app/layout.jsx
@@ -0,0 +1,30 @@
+import { Inter } from 'next/font/google';
+import './globals.css';
+import { ThemeProvider } from '@/components/theme-provider';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata = {
+  title: 'Muse AI',
+  description: 'AI Powered Conent Creation',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang='en' suppressHydrationWarning>
+      <body className={`${inter.className}`}>
+        <ThemeProvider
+          attribute='class'
+          defaultTheme='dark'
+          enableSystem
+          disableTransitionOnChange
+        >
+          {/* header */}
+          <main className='bg-slate-900 min-h-screen text-white overflow-x-hidden'>
+            {children}
+          </main>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
